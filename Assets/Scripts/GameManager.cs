b5761eedf4b956e3e8634d562d5c8e21aa7c4751
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-09-05 21:26:34 -0400

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+
+    void Start()
+    {
+        Instantiate(enemyPrefab);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/target.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target : MonoBehaviour
+{
+
+    void OnEnable()
+    {
+        Die();
+    }
+
+    void Die()
+    {
+        Destroy(gameObject, 2);
+    }
+
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+}
